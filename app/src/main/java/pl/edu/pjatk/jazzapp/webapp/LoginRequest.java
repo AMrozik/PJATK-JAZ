db9d68a3d8483bcf39df1c8d24fb7898ee790427
AMrozik <andrzej.mrozik98@gmail.com>
2019-10-16 14:11:35 +0200

    test commit from intellij

--- app/src/main/java/pl/edu/pjatk/jazzapp/webapp/LoginRequest.java
+++ app/src/main/java/pl/edu/pjatk/jazzapp/webapp/LoginRequest.java
@@ -2,6 +2,8 @@ package pl.edu.pjatk.jazzapp.webapp;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Named
 @RequestScoped
@@ -25,4 +27,23 @@ public class LoginRequest {
     public void setUsername(String username) {
         this.username = username;
     }
+
+    public boolean CheckEmail(String email) {
+        String regex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    public boolean CheckDate(String email) {
+        String regex = "^[0-9]{2}/[0-9]{2}/[0-9]{4}$";
+
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
 }
